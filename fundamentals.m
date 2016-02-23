397ce18d320c063931b2f391522c0950f325be04
google.username <alibarker89@gmail.com>
2016-02-23 12:02:44 +0000

    2nd change

---
+++ fundamentals.m
@@ -0,0 +1,8 @@
+function [ f0 B ] = fundamentals(audioFile, transcriptionMatrix)
+%FUNDAMENTALS Returns a vector of fundamental frequencies of notes provided
+%by the transcription matrix
+
+
+
+end
+
